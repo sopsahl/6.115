915af330d3daeac6e589a44ba5de7fd746621f90
Simon Opsahl <107520201+sopsahl@users.noreply.github.com>
2024-05-01 11:07:07 -0400

    Sax Notes

--- 6.115FP/playnotes.m
+++ 6.115FP/playnotes.m
@@ -6,12 +6,14 @@ function [beginning, middle, ending, freq] = playnotes()
     freqs = [103.8	110.0	116.5	123.5 130.8	138.6	146.8	155.6	164.8	174.6	185.0	196.0	207.7	220.0	233.1	246.9 261.6	277.2	293.7	311.1	329.6	349.2	370.0	392.0	415.3	440.0	466.2	493.9];
     for i = 1:length(freqs)
         f = freqs(i)/392;
-        [beginning, middle, ending, freq(i)] = saxnotes(f, 10, 2317, 2372, 10);
+        [beginning, middle, ending, freq(i)] = saxnotes(f, 100, 2317, 2372, 10);
         % pause(0.5);
 
     end
-    writematrix(freq, "freq");
-    writematrix(beginning, "beginning");
-    writematrix(middle, "middle");
-    writematrix(ending, "ending");
+    % writematrix(transpose(freq), "freq");
+    % writematrix(beginning, "beginning");
+    % writematrix(middle, "middle");
+    % writematrix(ending, "ending");
+    periods = round(24000000*1./freq);
+    writematrix(transpose(periods), "periods");
 end
